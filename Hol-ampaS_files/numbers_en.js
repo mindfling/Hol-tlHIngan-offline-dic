72931c78a3b6f6ec025c35c70168d333e021d492
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-07-02 08:05:41 +0300

    add current words

---
+++ Hol-ampaS_files/numbers_en.js
@@ -0,0 +1,125 @@
+Klingon Numbers:
+
+```
+wa' - one
+cha' - two
+wej - three
+loS - four
+vagh - five
+jav - six
+Soch - seven
+chorgh - eight
+Hut - nine
+```
+
+```
+wa',
+cha',
+wej,
+loS,
+vagh,
+jav,
+Soch,
+chorgh,
+Hut,
+
+wa'maH,
+wa'maH wa',
+wa'maH cha',
+wa'maH wej,
+wa'maH loS,
+wa'maH vagh,
+wa'maH jav,
+wa'maH Soch,
+wa'maH chorgh,
+wa'maH Hut,
+
+cha'maH,
+cha'maH wa',
+cha'maH cha',
+cha'maH wej,
+cha'maH loS,
+cha'maH vagh,
+cha'maH jav,
+cha'maH Soch,
+cha'maH chorgh,
+cha'maH Hut,
+
+wejmaH,
+wejmaH wa',
+wejmaH cha',
+wejmaH wej,
+wejmaH loS,
+wejmaH vagh,
+wejmaH jav,
+wejmaH Soch,
+wejmaH chorgh,
+wejmaH Hut,
+
+loSmaH,
+loSmaH wa',
+loSmaH cha',
+loSmaH wej,
+loSmaH loS,
+loSmaH vagh,
+loSmaH jav,
+loSmaH Soch,
+loSmaH chorgh,
+loSmaH Hut,
+
+vaghmaH,
+vaghmaH wa',
+vaghmaH cha',
+vaghmaH wej,
+vaghmaH loS,
+vaghmaH vagh,
+vaghmaH jav,
+vaghmaH Soch,
+vaghmaH chorgh,
+vaghmaH Hut,
+
+javmaH,
+javmaH wa',
+javmaH cha',
+javmaH wej,
+javmaH loS,
+javmaH vagh,
+javmaH jav,
+javmaH Soch,
+javmaH chorgh,
+javmaH Hut,
+
+SochmaH,
+SochmaH wa',
+SochmaH cha',
+SochmaH wej,
+SochmaH loS,
+SochmaH vagh,
+SochmaH jav,
+SochmaH Soch,
+SochmaH chorgh,
+SochmaH Hut,
+
+chorghmaH,
+chorghmaH wa',
+chorghmaH cha',
+chorghmaH wej,
+chorghmaH loS,
+chorghmaH vagh,
+chorghmaH jav,
+chorghmaH Soch,
+chorghmaH chorgh,
+chorghmaH Hut,
+
+HutmaH,
+HutmaH wa',
+HutmaH cha',
+HutmaH wej,
+HutmaH loS,
+HutmaH vagh,
+HutmaH jav,
+HutmaH Soch,
+HutmaH chorgh,
+HutmaH Hut
+```
+
